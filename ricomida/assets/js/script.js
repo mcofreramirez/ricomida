debe478097308efa37963639bd51e469aa79e86b
Manuel Cofré <96482222+mcofreramirez@users.noreply.github.com>
2022-02-04 09:42:48 -0300

    Desafio Ricomida
    Curso FULLSTACK PYTHON 008 Edutecno

---
+++ ricomida/assets/js/script.js
@@ -0,0 +1,62 @@
+$(function() { 
+
+$("#enviarCorreo").click(function(){
+
+alert("El Correo fue enviado correctamente ")
+
+
+})
+
+$("#favoritos").click(function(){
+
+    alert("Se añadio a favoritos ")
+    
+    
+    })
+
+    $(function(){
+
+$("#titulo1").click(function(){
+
+    $("#text1").toggle();
+})
+
+    })
+
+
+    $(function(){
+
+    $("#titulo2").click(function(){
+        
+    $("#text2").toggle();
+ })
+        
+    })
+
+    $(function(){
+
+    $("#titulo3").click(function(){
+                
+    $("#text3").toggle();
+})
+                
+    })
+
+
+$("body").on("dblclick", "h4" , function(){
+
+
+$(this).css("color", "red");
+
+})
+
+
+    
+
+
+})
+
+
+
+
+
